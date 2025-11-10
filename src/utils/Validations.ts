cd877c3bf2cbb5e8193760d7d4e96a4422992a92
Aniruddha Gade <gadeaniruddha2@gmail.com>
2025-11-10 22:54:16 +0530

    feat: validation functions

---
+++ src/utils/Validations.ts
@@ -0,0 +1,11 @@
+export function notNullUndefined(value: any): boolean {
+  return value !== null && value !== undefined;
+}
+
+export function isArrayLength(array: any): boolean {
+  return isArray(array) && array.length > 0;
+}
+
+export function isArray(array: any): boolean {
+  return notNullUndefined(array) && Array.isArray(array);
+}
