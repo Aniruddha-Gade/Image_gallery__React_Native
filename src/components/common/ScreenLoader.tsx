f6924d4662fa1a32c7c4987f43e73eff3af91457
Aniruddha Gade <gadeaniruddha2@gmail.com>
2025-11-10 22:50:40 +0530

    feat: screen loader

---
+++ src/components/common/ScreenLoader.tsx
@@ -0,0 +1,24 @@
+import { View, Modal, ActivityIndicator, StyleSheet } from 'react-native';
+import React from 'react';
+import Color from '../../assets/Color';
+
+function ScreenLoader({ loader }: { loader: boolean }) {
+  return (
+    <Modal transparent visible={loader} animationType="fade">
+      <View style={styles.container}>
+        <ActivityIndicator color={Color.primary} size="large" />
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: Color.light_grey_1,
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default ScreenLoader;
