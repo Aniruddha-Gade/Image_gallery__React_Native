fd8ccc758642e42f69decb5e95ee0af6a54e26a5
Aniruddha Gade <gadeaniruddha2@gmail.com>
2025-11-13 01:02:16 +0530

    feat: task screen

--- src/screens/task-list/TaskScreen.tsx
+++ src/screens/task-list/TaskScreen.tsx
@@ -1,33 +1,13 @@
-import { ScrollView, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import Color from '../../assets/Color';
-import IconButton from '../../components/common/IconButton';
-import useTask from './components/hooks/useTask';
-import OverlayModal from '../../components/common/OverlayModal';
-import { LABEL } from './constant/constant';
-import AddTaskForm from './components/AddTaskForm';
+import TaskListing from './components/TaskListing';
 
 const TaskScreen = () => {
-  const { loading, tasks, isAddModalVisible, openAddModal, closeAddModal } =
-    useTask();
-
   return (
-    <ScrollView style={styles.container}>
-      {/* Add Image Button */}
-      <IconButton
-        onPress={openAddModal}
-        name="plus"
-        icon="AntDesign"
-        customStyles={styles.addBtn}
-      />
-
-      <OverlayModal
-        title={LABEL.ADD_TASK}
-        visibility={isAddModalVisible}
-        onClose={closeAddModal}
-        InnerContent={<AddTaskForm closeModal={closeAddModal} />}
-      />
-    </ScrollView>
+    <View style={styles.container}>
+      <TaskListing />
+    </View>
   );
 };
 
@@ -37,8 +17,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Color.black,
-    paddingHorizontal: 16,
-    paddingTop: 50,
   },
   addBtn: {
     position: 'absolute',
