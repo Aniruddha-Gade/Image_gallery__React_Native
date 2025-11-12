b337e308fa647903a2c0f078581d6946bf5f53e0
Aniruddha Gade <gadeaniruddha2@gmail.com>
2025-11-13 00:44:45 +0530

    feat: wrapp app with redux provider and add toast component

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+import 'react-native-get-random-values';
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,23 +6,26 @@
  * @format
  */
 
-import { StatusBar, useColorScheme } from 'react-native';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import RootNavigation from './src/navigations/RootNavigation';
 import { PaperProvider } from 'react-native-paper';
+import { Provider } from 'react-redux';
+import { store } from './src/app/store';
+import Toast from 'react-native-toast-message';
+import { toastConfig } from './src/utils/toastConfig';
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
   return (
-    <SafeAreaProvider>
-      <SafeAreaView style={{ flex: 1 }}>
-        <PaperProvider>
-          <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-          <RootNavigation />
-        </PaperProvider>
-      </SafeAreaView>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <SafeAreaView style={{ flex: 1 }}>
+          <PaperProvider>
+            <RootNavigation />
+            <Toast config={toastConfig} position="bottom" />
+          </PaperProvider>
+        </SafeAreaView>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
