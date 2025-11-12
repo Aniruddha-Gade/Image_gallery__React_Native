2ff8aefab2ad7546b12e017f9e4e9d30ad616822
Aniruddha Gade <gadeaniruddha2@gmail.com>
2025-11-13 01:03:13 +0530

    feat: remove date schema from add task

--- src/screens/task-list/schema/AddTaskSchema.ts
+++ src/screens/task-list/schema/AddTaskSchema.ts
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { boolean, date, string } from 'yup';
+import { boolean, string } from 'yup';
 import { ERR } from './validationMsg';
 
 export const addTaskSchema = Yup.object().shape({
@@ -12,5 +12,5 @@ export const addTaskSchema = Yup.object().shape({
 
   status: boolean().required(ERR.status.required),
 
-  date: date().required(ERR.date.required).typeError(ERR.date.type),
+  // date: date().required(ERR.date.required).typeError(ERR.date.type),
 });
