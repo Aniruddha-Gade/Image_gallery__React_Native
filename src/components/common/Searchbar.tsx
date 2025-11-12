d7141a84b42041f119bae13d96440b558eb934ac
Aniruddha Gade <gadeaniruddha2@gmail.com>
2025-11-13 03:43:22 +0530

    feat: add clear all feature in search bar

--- src/components/common/Searchbar.tsx
+++ src/components/common/Searchbar.tsx
@@ -1,7 +1,9 @@
-import { StyleSheet } from 'react-native';
 import React from 'react';
+import { View, StyleSheet, TouchableOpacity } from 'react-native';
 import TextField from './TextField';
 import Color from '../../assets/Color';
+import { notEmpty } from '../../utils/Validations';
+import { CustomIcon } from './CustomIcon';
 
 type Props = {
   searchQuery: string;
@@ -10,21 +12,46 @@ type Props = {
 
 const Searchbar = ({ searchQuery, setSearchQuery }: Readonly<Props>) => {
   return (
-    <TextField
-      placeholder="Search tasks..."
-      value={searchQuery}
-      style={styles.searchInput}
-      onChangeText={setSearchQuery}
-    />
+    <View style={styles.container}>
+      {/* Input field */}
+      <TextField
+        placeholder="Search tasks..."
+        value={searchQuery}
+        style={styles.searchInput}
+        onChangeText={setSearchQuery}
+      />
+
+      {/* Cross icon inside input */}
+      {notEmpty(searchQuery) && (
+        <TouchableOpacity
+          onPress={() => setSearchQuery('')}
+          style={styles.clearIcon}
+          hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
+        >
+          <CustomIcon icon="Octicons" name="x" color={Color.red} size={20} />
+        </TouchableOpacity>
+      )}
+    </View>
   );
 };
 
 export default Searchbar;
 
 const styles = StyleSheet.create({
+  container: {
+    position: 'relative',
+    justifyContent: 'center',
+  },
   searchInput: {
     padding: 10,
+    paddingRight: 35, // space for the clear icon
     width: '100%',
     color: Color.white_1,
+  },
+  clearIcon: {
+    position: 'absolute',
+    right: 15,
+    top: '50%',
+    transform: [{ translateY: -15 }], // center vertically
   },
 });
