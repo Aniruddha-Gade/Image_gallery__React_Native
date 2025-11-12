8cab24dab5171c0f6fe6cc05a7460d01f6c199d4
Aniruddha Gade <gadeaniruddha2@gmail.com>
2025-11-13 01:05:34 +0530

    feat: constant labels for task

--- src/screens/task-list/constant/constant.ts
+++ src/screens/task-list/constant/constant.ts
@@ -1,7 +1,8 @@
-import { Task } from "../types/type";
+import { SYNC_STATUS, Task } from '../types/type';
 
 export const LABEL = {
   ADD_TASK: 'Add Task',
+  EDIT_TASK: 'Edit Task',
   TASK_LIST: 'Task List',
   NO_DATA: 'No task found \n Please add task',
   TASK_TITLE: 'Task Name',
@@ -10,11 +11,21 @@ export const LABEL = {
   ENTER_TASK_DESCRIPTION: 'Enter task description',
   TASK_STATUS: 'Completion Status',
   SUBMIT: 'Add Task',
+  UPDATE_TASK: 'Update Task',
+  ADD_TASK_SUCCESS: 'Task added successfully',
+  UPDATE_TASK_SUCCESS: 'Task updated successfully',
 };
 
 export const ADD_TASK_INIT_VALUES: Task = {
   title: '',
   description: '',
   status: false,
-  date: new Date(),
+  completed: false,
+  createdAt: '',
+  updatedAt: '',
+  syncStatus: SYNC_STATUS.PENDING,
+  remoteId: null,
+  id: '',
 };
+
+export const TASKS_KEY = 'MYAPP_TASKS_V1';
